c84b8233086bb7ac839f4e6b480d2d8716e940cf
yangqiang <892054096@qq.com>
2019-11-21 19:06:03 +0800

    提交dubbo-admin-server

---
+++ spring-mybatisplus/src/main/java/com/springframework/sf/mybatisplus/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.springframework.sf.mybatisplus.service;
+
+import com.springframework.sf.mybatisplus.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yq
+ * @since 2019-11-21
+ */
+public interface IUserService extends IService<User> {
+
+}
